97924ceb127996d3d1572de8c2cc795ed5743d54
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-13 12:00:58 +0300

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.lenth;
-if (args === 2){
+if (args == 2){
     console.log('No argument');
-}else if (args === 3){
-    console.log('Argument found');
 }else{
     console.log('Arguments found');
 }
