091375d9b673c094cae1675452a47c8b22c8aaeb
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-13 10:40:07 +0000

    loopOfLanguages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let string of strings) {
+for (const string of strings) {
   console.log(string);
 }
