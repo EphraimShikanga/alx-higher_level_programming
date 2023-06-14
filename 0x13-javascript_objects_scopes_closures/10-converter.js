166fcc514a2ec94618ff172d62e67daedd3e4b9c
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-14 09:51:31 +0300

    numberConversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.converter = function (base) {
+  return function myConvert (number) {
+    if (number === 0) {
+      return '';
+    } else {
+      return myConvert(Math.floor(number / base)) + (number % base);
+    }
+  };
+};
