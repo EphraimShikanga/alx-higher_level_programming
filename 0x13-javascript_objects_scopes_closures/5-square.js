8574cc2bfc9ad8ca157300f06f77bc8a40267bf1
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-14 08:54:31 +0300

    square#1

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tempHeight = this.height;
+    this.height = this.width;
+    this.width = tempHeight;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
