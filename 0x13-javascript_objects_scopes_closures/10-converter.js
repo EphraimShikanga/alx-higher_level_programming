839954625fd74c53133de08a772b3c99a0de9dad
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-14 10:02:05 +0300

    numberConversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,18 +1,15 @@
+#!/usr/bin/node
+
 exports.converter = function (base) {
-    return function convertToBaseN(number) {
-      if (number === 0) {
-        return '0';
-      }
-  
-      let result = '';
-  
-      while (number > 0) {
-        const remainder = number % base;
-        const digit = remainder < 10 ? remainder : String.fromCharCode(remainder + 55);
-        result = digit + result;
-        number = Math.floor(number / base);
-      }
-  
-      return result;
-    };
+  return function convertToBaseN (number) {
+    if (number === 0) {
+      return '0';
+    }
+
+    if (number < base) {
+      return number < 10 ? number.toString() : String.fromCharCode(number + 55);
+    }
+
+    return convertToBaseN(Math.floor(number / base)) + convertToBaseN(number % base);
   };
+};
