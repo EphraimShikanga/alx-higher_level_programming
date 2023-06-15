5e43af51e91b8b9a5c12d313c23e64cfa2a290d5
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-15 09:09:50 -0700

    correction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
 -- inserts new rows 
-INSERT INTO first_table (id, name) VALUES (89, 'Best School'),(89, 'Best School'),(89, 'Best School');
+INSERT INTO first_table (id, name)VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
 
