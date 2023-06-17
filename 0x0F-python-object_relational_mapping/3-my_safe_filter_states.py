863f4e96ab12bd62648930a5b10cd2e7fe9d2537
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-17 17:24:47 +0300

    my_safe_filter_states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,13 +8,11 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    # Retrieve MySQL connection parameters
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -25,18 +23,10 @@ if __name__ == '__main__':
 
     # Create a cursor object to execute SQL queries
     cursor = db.cursor()
-
-    # Execute the query to retrieve all states
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Print the states
-    for row in rows:
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+    for row in cursor.fetchall():
         print(row)
 
-    # Close the cursor and database connection
     cursor.close()
     db.close()
-
