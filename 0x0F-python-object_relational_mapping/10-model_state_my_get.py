16343cbffc2f19f2920f25c96d7a8e7f2c9dcf09
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-17 17:53:49 +0300

    model_state_my_get

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,13 +15,12 @@ if __name__ == '__main__':
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Create engine and bind to the database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database for the State object with the given name
     state = session.query(State).filter(State.name == state_name).first()
     if state is None:
         print("Not found")
