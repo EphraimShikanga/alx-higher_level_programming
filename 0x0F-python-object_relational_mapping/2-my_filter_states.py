03db6bdadb6fff3f2dad56b70bc01890ea646708
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-17 05:26:15 -0700

    filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+displays the value inthe states table of hbtn_0e_0_usa 
+where name matches the argument
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
+    searched=sys.argv[4].strip("'")
+    cursor = db.cursor()
+    cursor.execute(
+            """SELECT * FROM states 
+            WHERE name LIKE '{}' ORDER BY id 
+            ASC""".format(searched))
+    [print(state) for state in cursor.fetchall()]
+    cursor.close()
+    db.close()
