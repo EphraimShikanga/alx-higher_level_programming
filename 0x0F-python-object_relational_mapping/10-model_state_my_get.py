c4c919d36d8d7a02dd3b65471f625f15d7148376
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-17 16:19:39 +0300

    getastate

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,4 +18,38 @@ if __name__ == "__main__":
     session = Session()
     states = session.query(State).filter(State.name.like('%a%'))
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))#!/usr/bin/python3
+"""
+Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    # Retrieve MySQL connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create engine and bind to the database
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database for the State object with the given name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    # Print the result
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    # Close the session
+    session.close()
