9fb713b30f8ddcf195f6a4375c82afbc7254e9c2
Ephraim Shikanga <ephraimshikanga@gmail.com>
2023-06-17 14:22:02 +0300

    select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all states in the database
+lists all states from the database hbtn_0e_0_usa
+in Ascending order
 """
 
 import sys
@@ -18,14 +19,16 @@ if __name__ == '__main__':
             passwd = password,
             db = db_name
         )
+    # Create a cursor object to execute SQL queries
     cursor_object = db.cursor()
+    # Execute the query to retrieve all states
     cursor_object.execute("SELECT * FROM states ORDER BY id ASC")
+    #fetch all rows and prints the states
     rows = cursor_object.fetchall()
-
     for row in rows:
         state_id, state_name = row
         print(f"({state_id}, '{state_name}')")
-
+    #close db 
     cursor_object.close()
     db.close()
 
