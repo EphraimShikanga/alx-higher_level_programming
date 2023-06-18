33304169af1ed2968038d7a896c7a7ff3fb8c788
EphraimShikanga <ephraimshikanga@gmail.com>
2023-06-18 18:38:57 +0300

    test_base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+
+"""
+Unittest for base model
+"""
+
+import unittest
+from my_module import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_constructor_with_id(self):
+        obj = Base(10)
+        self.assertEqual(obj.id, 10)
+
+    def test_constructor_without_id(self):
+        obj1 = Base()
+        obj2 = Base()
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
