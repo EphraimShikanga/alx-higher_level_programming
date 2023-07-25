1a24ab295a6e5467f4a49a346dad01b6cd25c7fd
EphraimShikanga <ephraimshikanga@gmail.com>
2023-07-25 20:33:57 +0300

    read_file

--- 0x14-javascript-web_scrapping/0-readme.js
+++ 0x14-javascript-web_scrapping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.error('Error reading file:', err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.error('Error reading file:', err);
+  } else {
+    console.log(data);
+  }
 });
-
