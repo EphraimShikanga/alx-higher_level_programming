c0169a45bdc33f583b81755bc6f67488383628db
EphraimShikanga <ephraimshikanga@gmail.com>
2023-07-25 23:22:26 +0300

    completed_tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request({ url: apiUrl, json: true }, (error, response, body) => {
+  if (error) {
+    return console.error('Unable to connect to API:', error);
+  } else if (response.statusCode !== 200) {
+    return console.error('Status Code:', response.statusCode);
+  }
+
+  const users = {};
+
+  body.forEach(task => {
+    if (task.completed) {
+      const userId = task.userId;
+      if (users[userId]) {
+        users[userId]++;
+      } else {
+        users[userId] = 1;
+      }
+    }
+  });
+
+  for (const [userId, count] of Object.entries(users)) {
+    console.log(`${userId}: ${count}`);
+  }
+});
