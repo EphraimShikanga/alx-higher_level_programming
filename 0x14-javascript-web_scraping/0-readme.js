d993bee74d300c4d995cfc589797b609dc730e32
EphraimShikanga <ephraimshikanga@gmail.com>
2023-08-14 19:06:42 +0300

    updates_text_color

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,6 +6,6 @@ fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
     console.error('Error reading file:', err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
